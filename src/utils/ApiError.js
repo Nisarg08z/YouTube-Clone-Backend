2906f2daf04c6b73ef3d965fb76dde9924d7e793
Nisarg08z <nisarg693z@gmail.com>
2024-12-22 16:04:46 +0530

    day-4.2

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -12,7 +12,7 @@ class ApiError extends Error {
         this.success = false;
         this.error = error;
 
-        if(statck) {
+        if(stack) {
             this.stack = stack;
         } else {
             Error.captureStackTrace(this, this.constructor);
