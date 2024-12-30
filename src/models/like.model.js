c7e07f77c860806fabd249bef07124d9e34b0fe6
Nisarg08z <nisarg693z@gmail.com>
2024-12-30 09:18:16 +0530

    day-10.0

--- src/models/like.model.js
+++ src/models/like.model.js
@@ -1,5 +1,22 @@
 import mongoose, {Schema} from 'mongoose'
 
-const likeSchema = new Schema({},{timestamps: true})
+const likeSchema = new Schema({
+    comment: {
+        type: Schema.Types.ObjectId,
+        ref: 'Comment'
+    },
+    video: {
+        type: Schema.Types.ObjectId,
+        ref: 'Video'
+    },
+    tweet: {
+        type: Schema.Types.ObjectId,
+        ref: 'Tweet'
+    },
+    likedBy: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+},{timestamps: true})
 
 export const Like = mongoose.model("Liek", likeSchema)
