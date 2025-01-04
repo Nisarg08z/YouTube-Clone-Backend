e02b6fa1a9e8d3ad93eb10f71bcfa387e37ea640
Nisarg08z <nisarg693z@gmail.com>
2025-01-04 17:14:07 +0530

    day-13.4

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -4,7 +4,6 @@ import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
 
-
 const createPlaylist = asyncHandler(async (req, res) => {
     const { name, description } = req.body;
 
@@ -23,7 +22,41 @@ const createPlaylist = asyncHandler(async (req, res) => {
         .json(new ApiResponse(true, "Playlist created successfully", playlist));
 });
 
+const getUserPlaylists = asyncHandler(async (req, res) => {
+    const { userId } = req.params;
+
+    if (!isValidObjectId(userId)) {
+        throw new ApiError(400, "Invalid user ID");
+    }
+
+    const playlists = await Playlist.find({ owner: userId });
+
+    return res
+        .status(200)
+        .json(new ApiResponse(true, "User playlists fetched successfully", playlists));
+});
+
+const getPlaylistById = asyncHandler(async (req, res) => {
+    const { playlistId } = req.params;
+
+    if (!isValidObjectId(playlistId)) {
+        throw new ApiError(400, "Invalid playlist ID");
+    }
+
+    const playlist = await Playlist.findById(playlistId);
+
+    if (!playlist) {
+        throw new ApiError(404, "Playlist not found");
+    }
+
+    return res
+        .status(200)
+        .json(new ApiResponse(true, "Playlist fetched successfully", playlist));
+});
+
 
 export {
     createPlaylist,
+    getUserPlaylists,
+    getPlaylistById
 }
