e32e486ff76b301dc2623ee82646dcd82a5b7704
Nisarg08z <nisarg693z@gmail.com>
2025-01-18 21:56:57 +0530

    day-17.1

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -51,33 +51,24 @@ const registerUser = asyncHandler( async (req, res) => {
     if (existedUser) {
         throw new ApiError(409, "User with email or username already exists")
     }
-    //console.log(req.files);
-
-    const avatarLocalPath = req.files?.avatar[0]?.path;
-    //const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
     let coverImageLocalPath;
     if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
         coverImageLocalPath = req.files.coverImage[0].path
     }
-    
 
-    if (!avatarLocalPath) {
-        throw new ApiError(400, "Avatar file is required")
+    let avatarLocalPath;
+    if (req.files && Array.isArray(req.files.avatar) && req.files.avatar.length > 0) {
+        avatarLocalPath = req.files.avatar[0].path
     }
-
+    
     const avatar = await uploadOnCloudinary(avatarLocalPath)
     const coverImage = await uploadOnCloudinary(coverImageLocalPath)
 
-    if (!avatar) {
-        throw new ApiError(400, "Avatar file is required")
-    }
-   
-
     const user = await User.create({
         fullName,
-        avatar: avatar.url,
-        coverImage: coverImage?.url || "",
+        avatar: avatar?.url || "https://res.cloudinary.com/dby0edrrn/image/upload/v1737217499/cgsf8wnhxr5s0z96lpvg.jpg",
+        coverImage: coverImage?.url || "https://res.cloudinary.com/dby0edrrn/image/upload/v1737217521/a1duxlv7ztfcnatupr0l.jpg",
         email, 
         password,
         username: username.toLowerCase()
