18f347dfb76695391db41abba82cef09654174e8
Nisarg08z <nisarg693z@gmail.com>
2025-02-21 00:16:00 +0530

    day-33.0

--- src/routes/subscription.route.js
+++ src/routes/subscription.route.js
@@ -8,15 +8,14 @@ import {
 import {verifyJWT} from "../middlewares/auth.middleware.js"
 
 const router = Router();
-router.use(verifyJWT); 
 
-router.route("/c/:channelId/toggle").post(toggleSubscription);
+router.route("/c/:channelId/toggle").post(verifyJWT, toggleSubscription);
 
 router.route("/c/:channelId/subscribers").get(getUserChannelSubscribers);
 
-router.route("/user/subscribed").get(getSubscribedChannels);
+router.route("/user/subscribed").get(verifyJWT, getSubscribedChannels);
 
-router.route("/check/c/:channelId").post(isChannelFollowed);
+router.route("/check/c/:channelId").post(verifyJWT, isChannelFollowed);
 
 
 export default router
